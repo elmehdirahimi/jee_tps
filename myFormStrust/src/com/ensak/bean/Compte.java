f7b406a08ae5d928a2569f38c28309a7905dbfbf
elmehdirahimi <elmehdirahimi@gmail.com>
2021-01-20 10:48:24 +0100

    exam2

--- myFormStrust/src/com/ensak/bean/Compte.java
+++ myFormStrust/src/com/ensak/bean/Compte.java
@@ -1,59 +1,84 @@
 package com.ensak.bean;
 
+import java.io.Console;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.ensak.HibernateUtil;
 import com.ensak.dao.Client;
 
+
+
 public class Compte {
 	
-	
+	int code;
+	String nom;
+	String ville;
+
+	public Compte(int code, String nom, String ville) {
+	super();
+	this.code = code;
+	this.nom = nom;
+	this.ville = ville;
+}
+
+
+	public int getCode() {
+	return code;
+}
+
+
+public void setCode(int code) {
+	this.code = code;
+}
+
+
+public String getNom() {
+	return nom;
+}
+
+
+public void setNom(String nom) {
+	this.nom = nom;
+}
+
+
+public String getVille() {
+	return ville;
+}
+
+
+public void setVille(String ville) {
+	this.ville = ville;
+}
+
+
 	public Compte() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Compte(String nom, String prenom, String courriel, String password) {
-		super();
-		this.nom = nom;
-		this.prenom = prenom;
-		this.courriel = courriel;
-		this.password = password;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	public String getCourriel() {
-		return courriel;
-	}
-	public void setCourriel(String courriel) {
-		this.courriel = courriel;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 	
 	public String CreerCompte() {
 		
 		Session session = HibernateUtil.sessionFactory.openSession();
 		Transaction transaction=session.beginTransaction();		
-	
-		Client cl= new Client(1l, "mehdi", "tttt");
+		Client cl= new Client((long) code, nom, ville);
 		session.save(cl) ;
 		transaction.commit();
+		return "success";
+	}
+	
+	
+public String affichage() {
 		
+	SessionFactory SF = new Configuration().configure().buildSessionFactory();
+	Session session = SF.openSession();
+    org.hibernate.Query q = session.createQuery("from Etudiant");
+	return  q.list();
+	
 		return "success";
 	}
 	
